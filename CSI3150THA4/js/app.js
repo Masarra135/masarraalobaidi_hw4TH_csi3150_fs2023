eb179d7bba5f49f702bcad1b8efd597c94231509
Masarra135 <71594188+Masarra135@users.noreply.github.com>
2023-10-29 22:11:04 -0400

    commit

---
+++ CSI3150THA4/js/app.js
@@ -0,0 +1,34 @@
+const filterForm = document.getElementById("filterForm");
+const priceMinInput = document.getElementById("priceMin");
+const priceMaxInput = document.getElementById("priceMax");
+const makeFilter = document.getElementById("makeFilter");
+const colorFilter = document.getElementById("colorFilter");
+
+document.getElementById("filterButton").addEventListener("click", filterCards);
+
+function filterCards() {
+  const minPrice = parseFloat(priceMinInput.value);
+  const maxPrice = parseFloat(priceMaxInput.value);
+
+  const selectedMake = makeFilter.value;
+  const selectedColor = colorFilter.value;
+
+  const cards = document.querySelectorAll(".Cars");
+
+  cards.forEach((card) => {
+    const cardPrice = parseFloat(card.getAttribute("price"));
+    const cardMake = card.getAttribute("type");
+    const cardColor = card.getAttribute("color");
+
+    if (
+      cardPrice >= minPrice &&
+      cardPrice <= maxPrice &&
+      (selectedMake === "AllMakes" || cardMake === selectedMake) &&
+      (selectedColor === "AllColors" || cardColor === selectedColor)
+    ) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
